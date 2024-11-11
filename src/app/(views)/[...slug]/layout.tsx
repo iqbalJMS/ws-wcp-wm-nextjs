975302abacbaa85ebe4d125801d2f9460cedf753
ekyourkid <eky1579@gmail.com>
2024-11-11 17:01:20 +0700

    fix: footer props

--- src/app/(views)/[...slug]/layout.tsx
+++ src/app/(views)/[...slug]/layout.tsx
@@ -5,8 +5,9 @@ import GlobalFooter from '@/lib/element/global/global.footer';
 import { ACT_GetTopMenuNavbar } from '../$action/action.get.top-menu-navbar';
 import { ACT_GetMainMenuNavbar } from '../$action/action.get.main-menu-navbar';
 import { ACT_GetMenuItemNavbar } from '../$action/action.get-menu-item-navbar';
-import { ACT_GetMainMenuFooter } from '../$action/action.get.main-footer';
+
 import { ACT_GetBottomRightFooter } from '../$action/bottom-footer/action.get.bottom.right.footer';
+import { ACT_GetBottomLeftFooter } from '../$action/bottom-footer/action.get.bottom.left.footer';
 
 export default async function WmSlugLayout({
   children,
@@ -15,10 +16,9 @@ export default async function WmSlugLayout({
 }>) {
   const listHeaderTop = await ACT_GetTopMenuNavbar({ lang: 'en' });
   const listHeaderBottom = await ACT_GetMainMenuNavbar({ lang: 'en' });
-  const itemMenuLogin = await ACT_GetMenuItemNavbar({ lang: 'en' });
-
-  const listMainFooter = await ACT_GetMainMenuFooter({ lang: 'en' });
+  const listBottomLeftFooter = await ACT_GetBottomLeftFooter({ lang: 'en' });
   const listBottomRightFooter = await ACT_GetBottomRightFooter({ lang: 'en' });
+  const itemMenuLogin = await ACT_GetMenuItemNavbar({ lang: 'en' });
   return (
     <>
       <GlobalHeader
@@ -29,9 +29,11 @@ export default async function WmSlugLayout({
       />
       <main className="pb-10">{children}</main>
       <GlobalFooter
-        main_footer={listMainFooter}
-        bottom_footer={'listBottomFooter'}
+        main_footer={{
+          data: [],
+        }}
         bottom_right_footer={listBottomRightFooter}
+        bottom_left_footer={listBottomLeftFooter}
       />
     </>
   );
--- src/app/tester-component/layout.tsx
+++ src/app/tester-component/layout.tsx
@@ -1,4 +1,4 @@
-import GlobalFooter from '@/lib/element/global/global.footer';
+// import GlobalFooter from '@/lib/element/global/global.footer';
 
 import { Metadata } from 'next';
 import React from 'react';
@@ -6,8 +6,8 @@ import React from 'react';
 import GlobalHeader from '@/lib/element/global/global.header';
 import { ACT_GetTopMenuNavbar } from '@/app/(views)/$action/action.get.top-menu-navbar';
 import { ACT_GetMainMenuNavbar } from '@/app/(views)/$action/action.get.main-menu-navbar';
-import { ACT_GetMainMenuFooter } from '@/app/(views)/$action/action.get.main-footer';
-import { ACT_GetBottomMenuFooter } from '@/app/(views)/$action/action.get.bottom-footer';
+// import { ACT_GetMainMenuFooter } from '@/app/(views)/$action/action.get.main-footer';
+// import { ACT_GetBottomMenuFooter } from '@/app/(views)/$action/action.get.bottom-footer';
 import CE_MenuMain from '@/app/(views)/$element/client.menu.main';
 
 export const metadata: Metadata = {
@@ -22,8 +22,8 @@ export default async function AetherLayout({
   const listHeaderTop = await ACT_GetTopMenuNavbar({ lang: 'en' });
   const listHeaderBottom = await ACT_GetMainMenuNavbar({ lang: 'en' });
 
-  const listMainFooter = await ACT_GetMainMenuFooter({ lang: 'en' });
-  const listBottomFooter = await ACT_GetBottomMenuFooter({ lang: 'en' });
+  // const listMainFooter = await ACT_GetMainMenuFooter({ lang: 'en' });
+  // const listBottomFooter = await ACT_GetBottomMenuFooter({ lang: 'en' });
   return (
     <React.Fragment>
       <GlobalHeader
@@ -36,10 +36,10 @@ export default async function AetherLayout({
       <div className="h-screen bg-blue-300"></div>
 
       {children}
-      <GlobalFooter
+      {/* <GlobalFooter
         main_footer={listMainFooter}
         bottom_footer={listBottomFooter}
-      />
+      /> */}
     </React.Fragment>
   );
 }
