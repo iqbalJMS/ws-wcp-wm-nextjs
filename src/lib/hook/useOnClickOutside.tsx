ad6b926c4f816897e67dcee4805b1b82743cf96a
ichimarurei <jms21maru@gmail.com>
2025-05-21 11:41:43 +0700

    fix sonar

--- src/lib/hook/useOnClickOutside.tsx
+++ src/lib/hook/useOnClickOutside.tsx
@@ -6,17 +6,20 @@ const useOnClickOutside = (
   handler: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
+    if (typeof window === undefined) return;
+
     const listener = (event: MouseEvent | TouchEvent) => {
       if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
+
       handler(event);
     };
-    if (typeof window === undefined) return;
+
     document?.addEventListener('mousedown', listener);
     document?.addEventListener('touchstart', listener);
+
     return () => {
-      if (typeof window === undefined) return;
       document?.removeEventListener('mousedown', listener);
       document?.removeEventListener('touchstart', listener);
     };
